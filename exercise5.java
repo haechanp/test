640cb63374c3d1f669e70e8aaef5f8e731bf1427
haechanp <144800399+haechanp@users.noreply.github.com>
2023-09-19 21:39:18 +0900

    Add files via upload

---
+++ exercise5.java
@@ -0,0 +1,14 @@
+package homework1;
+
+public class exercise5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("문제5번 수식값");
+		System.out.println(67+12.8);
+		System.out.println(10/3);
+		System.out.println(10.0/3);
+		System.out.println(10 == 9);
+	}
+
+}

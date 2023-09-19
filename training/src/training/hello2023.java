9c9db2ff0ab6c19e96f7b70981ff6c045f15ea68
haechanp <144800399+haechanp@users.noreply.github.com>
2023-09-19 20:55:50 +0900

    Add files via upload

---
+++ training/src/training/hello2023.java
@@ -0,0 +1,16 @@
+package training;
+
+public class hello2023 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("welcome!!"); 
+
+System.out.println("자바 세계에 오신것을 환영합니다.");
+
+System.out.println("sorry~~");
+
+System.out.println("재밌어서 미안합니다~~");	
+	
+	}
+}

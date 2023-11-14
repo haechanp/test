8f69256002e785a25e077f4415c3a0374617904d
haechanp <144800399+haechanp@users.noreply.github.com>
2023-11-14 23:13:06 +0900

    Add files via upload

--- Act5/1~2/TV.java
+++ Act5/1~2/TV.java
@@ -23,10 +23,25 @@ class ColorTV extends TV {
     }
 }
 
-public class p0501 {
+class IPTV extends ColorTV {
+    private String ipAddress;
+
+    public IPTV(String ip, int size, int colorSize) {
+        super(size, colorSize);
+        ipAddress = ip;
+    }
+
+    @Override
+    public void printProperty() {
+        System.out.print("나의 IPTV는 " + ipAddress + " 주소의 ");
+        super.printProperty();
+    }
+}
+
+public class p0502 {
     public static void main(String[] args) {
-        ColorTV myTV = new ColorTV(32, 1024);
-        myTV.printProperty();
+        IPTV iptv = new IPTV("192.1.1.2", 32, 2048);
+        iptv.printProperty();
     }
 }
 
